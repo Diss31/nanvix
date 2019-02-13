c54b75444eaa6008a24c28842acfad20c7b9e97e
Diss <m.dumax-vorzet@orange.fr>
2019-02-13 10:49:46 +0100

    Revert "Correction et commentaire du code de sem.c" car pas tester la compilation avant de push
    This reverts commit 0409a9f1a6d21187c2df475ffb59927fb2b1ff84.

--- src/kernel/sys/sem.c
+++ src/kernel/sys/sem.c
@@ -4,42 +4,33 @@
 #include <nanvix/hal.h>
 #include <sys/sem.h>
 
-/**
- * @brief Create a new semaphore
- * 
- * @details Take the initial value of the semaphor et return the initializing semaphore
- *
-**/
 PUBLIC Semaphore create(int n){
 	Semaphore sem;
-	sem.val = n; 
+	sem.val = n; //assert n>0
 	sem.waiting_queue = NULL;
 	
 	return sem;
 }
 
-/**
- * @brief Decrease the value of the giving semaphore
- * 
- * @details If value become negative, we put the current process in sleep with a PRIO_SEM priority. All of that is in a critical section.
- *
-**/
+PUBLIC int print_value(Semaphore sem){
+	return sem.val;
+}
+
+PUBLIC Semaphore assign_value(Semaphore sem, int n){
+	sem.val=n;
+	return sem;
+}
+
 PUBLIC Semaphore down(Semaphore sem){
 	disable_interrupts();
 	sem.val--;
 	if(sem.val<0){		
-		sleep(sem.waiting_queue,PRIO_SEM);
+		sleep(sem.waiting_queue,curr_proc->priority);
 	}
 	enable_interrupts();
 	return sem;
 }
 
-/**
- * @brief Increase the value of the giving semaphore
- * 
- * @details If value become positive, we wake up the first process in sleep. All of that is in a critical section.
- *
-**/
 PUBLIC Semaphore up(Semaphore sem){
 	disable_interrupts();
 	sem.val++;
@@ -50,14 +41,8 @@ PUBLIC Semaphore up(Semaphore sem){
 	return sem;
 }
 
-/**
- * @brief Destroy the giving semaphore
- * 
- * @details We set the semaphore's value at DEAD_SEM and we flush its waiting_queue
- *
-**/
 PUBLIC Semaphore destroy(Semaphore sem){
-	sem.val=DEAD_SEM;
+	sem.val=-999;
 	sem.waiting_queue=NULL;
 	return sem;
 }
