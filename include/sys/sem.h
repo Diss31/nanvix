74785f9b468723b72e477f7e9428ef30ddcb9449
mastoura <iheb_mastoura1996@hotmail.com>
2019-02-13 09:22:41 +0100

    creation type unit + correction semget.c

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -1,18 +1,19 @@
+
 /*
  * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
+ *
  * This file is part of Nanvix.
- * 
+ *
  * Nanvix is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Nanvix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -36,6 +37,12 @@
 		struct process **waiting_queue;
 	} Semaphore;
 
+	typedef struct { //Suppression des PUBLIC et PRIVATE
+    Semaphore s;
+		unsigned key;
+		int nbproc;
+	} unit;
+
 	PUBLIC Semaphore create(int n);
 	PUBLIC int print_value(Semaphore sem);
 	PUBLIC Semaphore assign_value(Semaphore sem, int n);
@@ -45,7 +52,7 @@
 
 #define SIZE_MAX_TAB 50
 
-	PUBLIC Semaphore tab[SIZE_MAX_TAB][3];
+	PUBLIC unit tab[SIZE_MAX_TAB];
 
 /* Forward definitions. */
 	extern int semget(unsigned);
--- src/kernel/sys/sem/semget.c
+++ src/kernel/sys/sem/semget.c
@@ -4,33 +4,33 @@
  * @brief Creates a semaphore with the key if there's none assigned to this key already
  *
  * @details Search in the semaphore table if there's a semaphore with this key
- * 
+ *
  *          If yes, then we do nothing and return the key
  *	    If not, we create a semaphore with this key
- * 
+ *
  * @param key    The key of the semaphore
  *
  * @return key if everything went well, -1 if there was a problem
- */ 
+ */
 
 PUBLIC int sys_semget(unsigned key)
 {
 	int exists = 0;
 	for(int i=0; i<SIZE_MAX_TAB; i++){
-		if(tab[i][1]==key){
+		if(tab[i].key==key){
 			exists=1;
 			break;
 		}
 	}
 
 	if(!exists){
-		Semaphore s = create(0);
+		Semaphore semaphore = create(0);
 		/* ajouter le nouveau semaphore dans la table */
 		for(int i=0; i<SIZE_MAX_TAB; i++){
-			if(tab[i][0]==NULL){
-				tab[i][0]=s;
-				tab[i][1]=key;
-				tab[i][2]=0;
+			if(tab[i].s.waiting_queue==NULL){
+				tab[i].s=semaphore;
+				tab[i].key=key;
+				tab[i].nbproc=0;
 				break;
 			}
 		}
