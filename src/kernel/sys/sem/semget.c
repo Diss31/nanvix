5d67f41725bbc85966a626cc8d453f4a7ea51cbc
Hoel <jalmin.hoelle@gmail.com>
2019-02-13 11:59:57 +0100

    corrections du semget

--- src/kernel/sys/sem/semget.c
+++ src/kernel/sys/sem/semget.c
@@ -10,7 +10,7 @@
  *
  * @param key    The key of the semaphore
  *
- * @return key if everything went well, -1 if there was a problem
+ * @return The id of the semaphore in the table if everything went well, -1 if there was a problem
  */
 
 PUBLIC int sys_semget(unsigned key)
@@ -23,18 +23,19 @@ PUBLIC int sys_semget(unsigned key)
 		}
 	}
 
+	int id=0;
 	if(!exists){
-		Semaphore semaphore = create(0);
+		Semaphore semaphore = create(1);
 		/* ajouter le nouveau semaphore dans la table */
-		for(int i=0; i<SEM_SIZE_TAB; i++){
-			if(tab[i].s.waiting_queue==NULL){
-				tab[i].s=semaphore;
-				tab[i].key=key;
-				tab[i].nbproc=0;
+		for(;id<SIZE_MAX_TAB; id++){
+			if(tab[id].key==0){
+				tab[id].s=semaphore;
+				tab[id].key=key;
+				tab[id].nbproc=0;
 				break;
 			}
 		}
 	}
 
-	return key;
+	return id;
 }
