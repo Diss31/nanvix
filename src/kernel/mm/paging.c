eeb0f4bb292adbe6484d78a772c52202413d0508
Hoel <jalmin.hoelle@gmail.com>
2019-03-13 13:07:11 +0100

    clock première version

--- src/kernel/mm/paging.c
+++ src/kernel/mm/paging.c
@@ -284,6 +284,15 @@ PRIVATE struct
 	addr_t addr;    /**< Address of the page. */
 } frames[NR_FRAMES] = {{0, 0, 0, 0},  };
 
+int page_def_number=0;
+int pointer;
+
+void add_frame(void){
+	do{
+		pointer = (pointer+1)%NR_FRAMES;
+	} while(frames[pointer].owner != curr_proc->pid);
+}
+
 /**
  * @brief Allocates a page frame.
  * 
@@ -294,44 +303,63 @@ PRIVATE int allocf(void)
 {
 	int i;      /* Loop index.  */
 	int oldest; /* Oldest page. */
-	
+	struct pte *pg; /* Page table entry. */
 	#define OLDEST(x, y) (frames[x].age < frames[y].age)
-	
 	/* Search for a free frame. */
 	oldest = -1;
 	for (i = 0; i < NR_FRAMES; i++)
+	//for (i = ((curr_proc->last)+1)%NR_FRAMES; i != (curr_proc->last); i=(i+1)%NR_FRAMES) //on fait un tour du tableau à partir de la page après celle du processeur courant
 	{
 		/* Found it. */
-		if (frames[i].count == 0)
+		if (frames[i%NR_FRAMES].count == 0)
 			goto found;
-		
-		/* Local page replacement policy. */
-		if (frames[i].owner == curr_proc->pid)
-		{
-			/* Skip shared pages. */
-			if (frames[i].count > 1)
+	}
+	add_frame();
+	addr_t addr=frames[pointer].addr & PAGE_MASK;
+	pg = getpte(curr_proc, addr);
+
+	while(pg->accessed){
+		if (frames[pointer].count > 1)
 				continue;
+		pg->accessed=0;
+		if ((oldest < 0) || (OLDEST(pointer, oldest)))
+			oldest = pointer;
+	}
+
+		/* Local page replacement policy. */
+		/*if (frames[pointer].owner == curr_proc->pid)
+		{*/
+			/* Skip shared pages. */
 			
-			/* Oldest page found. */
-			if ((oldest < 0) || (OLDEST(i, oldest)))
-				oldest = i;
+
+			/*if(pg->accessed == 1){
+				pg->accessed = 0;
+			} else {*/
+				/* Oldest page found. */
+				/*if ((oldest < 0) || (OLDEST(pointer, oldest)))
+					oldest = pointer;
+			}
 		}
+	}*/
+
+	/* No frame left. */
+	if (oldest < 0){
+		return (-1);
 	}
 	
-	/* No frame left. */
-	if (oldest < 0)
-		return (-1);
-	
+	page_def_number++;
+	kprintf("D-%d", page_def_number);
 	/* Swap page out. */
-	if (swap_out(curr_proc, frames[i = oldest].addr))
+	if (swap_out(curr_proc, frames[i = oldest].addr)){
 		return (-1);
+	}
 	
 found:		
 
-	frames[i].age = ticks;
-	frames[i].count = 1;
+	frames[i%NR_FRAMES].age = ticks;
+	frames[i%NR_FRAMES].count = 1;
 	
-	return (i);
+	return (i%NR_FRAMES);
 }
 
 /**
